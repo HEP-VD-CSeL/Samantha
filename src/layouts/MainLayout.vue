946af22334ac76a4ed6e993e566bd550fc2f02e3
Marcel Grosjean <grosjean.marcel@gmail.com>
2025-05-04 11:47:19 +0200

    intial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click=""
+        />
+
+        <q-toolbar-title>
+          Samantha
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeMount } from 'vue';
+
+onBeforeMount(() => {
+  
+});
+
+onMounted(() => {
+
+});
+
+</script>
